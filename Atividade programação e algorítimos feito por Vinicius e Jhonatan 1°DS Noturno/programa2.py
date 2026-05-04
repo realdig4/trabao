68de64e68f4bb5d8828bfdeffbfb77d6511f700b
realdig4 <vrm63641@gmail.com>
2026-05-04 18:32:44 -0300

    Update programa2.py

--- Atividade programação e algorítimos feito por Vinicius e Jhonatan 1°DS Noturno/programa2.py
+++ Atividade programação e algorítimos feito por Vinicius e Jhonatan 1°DS Noturno/programa2.py
@@ -1,13 +1,11 @@
-# Entrada de dados
+
 nome_carro = input("Nome do automóvel: ")
 preco_fabrica = float(input("Preço de fábrica: R$ "))
 
-# Cálculos
 valor_impostos = preco_fabrica * 0.35
 lucro_revendedor = preco_fabrica * 0.15
 preco_final = preco_fabrica + valor_impostos + lucro_revendedor
 
-# Saída
 print("-" * 30)
 print(f"Automóvel: {nome_carro}")
 print(f"Preço de fábrica: R$ {preco_fabrica:.2f}")
