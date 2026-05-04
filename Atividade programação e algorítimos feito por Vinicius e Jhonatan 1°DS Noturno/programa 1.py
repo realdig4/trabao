0144464f7cef88eb082392a394b1fb322744e9bd
realdig4 <vrm63641@gmail.com>
2026-05-04 18:32:11 -0300

    Update programa 1.py

--- Atividade programação e algorítimos feito por Vinicius e Jhonatan 1°DS Noturno/programa 1.py
+++ Atividade programação e algorítimos feito por Vinicius e Jhonatan 1°DS Noturno/programa 1.py
@@ -1,15 +1,14 @@
-# Entrada das quantidades
+
 print("--- Padaria YYZ ---")
 paes = int(input("Quantidade de pães vendidos: "))
 broas = int(input("Quantidade de broas vendidas: "))
 
-# Cálculos
+
 valor_paes = paes * 0.12
 valor_broas = broas * 1.50
 total_vendas = valor_paes + valor_broas
 reserva_poupanca = total_vendas * 0.10
 
-# Saída de dados
 print("-" * 20)
 print(f"Total arrecadado: R$ {total_vendas:.2f}")
 print(f"Valor para poupança: R$ {reserva_poupanca:.2f}")
